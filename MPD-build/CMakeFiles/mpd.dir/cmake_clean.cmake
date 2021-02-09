aaa00318d803b679d96af36d4d97ce4705e85b2c
Silas-SJ <silasbn1@gmail.com>
2021-02-09 19:19:56 -0300

    First Commit

---
+++ MPD-build/CMakeFiles/mpd.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mpd.dir/mpd.cc.o"
+  "CMakeFiles/mpd.dir/src/mpdActionInitialization.cc.o"
+  "CMakeFiles/mpd.dir/src/mpdCalorHit.cc.o"
+  "CMakeFiles/mpd.dir/src/mpdCalorimeterSD.cc.o"
+  "CMakeFiles/mpd.dir/src/mpdDetectorConstruction.cc.o"
+  "CMakeFiles/mpd.dir/src/mpdEventAction.cc.o"
+  "CMakeFiles/mpd.dir/src/mpdPrimaryGeneratorAction.cc.o"
+  "CMakeFiles/mpd.dir/src/mpdRunAction.cc.o"
+  "mpd"
+  "mpd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mpd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
